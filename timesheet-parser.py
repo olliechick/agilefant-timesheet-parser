86ae7c46525ad507f5dce6bdad90a9bef334cdc6
Ollie Chick <och26@uclive.ac.nz>
2019-05-19 01:05:48 +1200

    Update timesheet-parser.py

--- timesheet-parser.py
+++ timesheet-parser.py
@@ -13,19 +13,20 @@ required_columns = {"Story", "Comment", "User", "Spent effort (hours)"}
 
 HELPTEXT = """
 Prints a list of users and how many hours they have logged.
-This is sorted by the number of hours logged, from least to most.
-By default, 1 decimal place is used (e.g. 12.5).
+By default, this is sorted by the number of hours logged, from least to most, and 1 decimal place is used (e.g. 12.5).
+It accepts command-line arguments, in the form ./timesheet-parser.py <command>, e.g. ./timesheet-parser.py a b c.
+My personal favourite is ./timesheet-parser.py r n, which produces a ranked list of who has logged the most hours.
 
 Possible args:
 
-a: use alphabetical order
-d[int]: use [int] decimal places (e.g. d3)
-f: use first names [Note that this will use the first name as a key, so multiple users with the same first name will be listed as one entry]
-h: print this help text
-n: include each user's ranking (a number; 1 -> n)
-r: print in reverse sorted order
-t: print all invalid log comments
-ta: print all log comments
+a: Sort users in alphabetical order (by first name)
+d[int]: Uses int decimal places (e.g. d3)
+f: Use first names (note that this means two users with the same first name get grouped together)
+h: Prints this help text (instead of running the main program)
+n: Include each user's ranking (a number; 1 -> n)
+r: Print in reverse sorted order
+t: Print all invalid log comments (for University of Canterbury course SENG302)
+ta: Print all log comments
 """
 
 
